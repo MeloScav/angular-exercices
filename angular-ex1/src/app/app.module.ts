7e4202c0d1a6a21a2449ae9bd9992e0bf319d5fe
Méloé <meloscavezzoni@gmail.com>
2020-07-12 23:49:32 +0200

    Added Posts Services

--- angular-ex1/src/app/app.module.ts
+++ angular-ex1/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 
+// SERVICES
+import { PostsService } from './services/posts.service';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './post-list/post-list.component';
@@ -27,7 +30,7 @@ const appRoutes: Routes = [
     PostFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
