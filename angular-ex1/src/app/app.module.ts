010ca4a0001a35a6fd4ee29a294cbd8e0420fa3d
Méloé <meloscavezzoni@gmail.com>
2020-07-12 22:04:28 +0200

    Added routing + header and post-form component

--- angular-ex1/src/app/app.module.ts
+++ angular-ex1/src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
+import { PostFormComponent } from './post-form/post-form.component';
+
+// Routing
+const appRoutes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: 'new', component: PostFormComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    HeaderComponent,
+    PostFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
