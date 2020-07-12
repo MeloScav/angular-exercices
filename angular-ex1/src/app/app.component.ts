f6e35d6074346e253d90c11637184f3c2a4e21eb
Méloé <meloscavezzoni@gmail.com>
2020-07-12 23:32:59 +0200

    Firebase initialization

--- angular-ex1/src/app/app.component.ts
+++ angular-ex1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,27 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // Tableau des posts
-  Posts = [
-    {
-      title: 'Mon premier post',
-      content:
-        'Nulla laboris consequat laboris voluptate nostrud ea in est ut.',
-      loveIts: 0,
-      theDate: new Date(),
-    },
-    {
-      title: 'Mon deuxième post',
-      content:
-        'Qui nulla aliquip duis Lorem excepteur laboris anim sunt dolore dolore aute officia est.',
-      loveIts: 0,
-      theDate: new Date(),
-    },
-    {
-      title: 'Encore un post',
-      content: 'Tempor ea minim dolor est pariatur id ullamco sit.',
-      loveIts: 0,
-      theDate: new Date(),
-    },
-  ];
+  constructor() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBN4Burf6FLvivrvHBoBdDeVum44QrFQv8',
+      authDomain: 'blog-ba63f.firebaseapp.com',
+      databaseURL: 'https://blog-ba63f.firebaseio.com',
+      projectId: 'blog-ba63f',
+      storageBucket: 'blog-ba63f.appspot.com',
+      messagingSenderId: '387133339912',
+      appId: '1:387133339912:web:e66732b2730b9e67fcd4bd',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
