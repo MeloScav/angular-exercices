05fd8505a2467e5f1d770dae6c6404f6326d1932
Méloé <meloscavezzoni@gmail.com>
2020-07-14 14:37:55 +0200

    Button new post

--- angular-ex1/src/app/post-list/post-list.component.ts
+++ angular-ex1/src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
@@ -6,9 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
-  @Input() posts: [];
-
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  // BUTTON NEW POST
+  onNewPost() {
+    this.router.navigate(['new']);
+  }
 }
