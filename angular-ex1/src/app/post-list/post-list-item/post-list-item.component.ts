0b859f65dc3b952e1a9d77e89b449ae842236411
Méloé <meloscavezzoni@gmail.com>
2020-07-14 14:41:11 +0200

    Subscription + update and get post + button delete

--- angular-ex1/src/app/post-list/post-list-item/post-list-item.component.ts
+++ angular-ex1/src/app/post-list/post-list-item/post-list-item.component.ts
@@ -1,33 +1,50 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { PostsService } from '../../services/posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.scss'],
 })
-export class PostListItemComponent implements OnInit {
-  @Input() postTitle: string;
-  @Input() postContent: string;
-  @Input() postLoveIts: number;
-  @Input() postDate: Date;
+export class PostListItemComponent implements OnInit, OnDestroy {
+  // Local array
+  posts: Post[];
+  // Subscription
+  postsSubscription: Subscription;
 
-  constructor() {}
+  constructor(private postsService: PostsService) {}
 
-  ngOnInit(): void {}
-
-  onLoveIt() {
-    this.postLoveIts = this.postLoveIts + 1;
+  ngOnInit(): void {
+    // Updates the local array
+    this.postsSubscription = this.postsService.postsSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.postsService.getPosts();
+    this.postsService.emitPosts();
   }
 
-  onDontLoveIt() {
-    this.postLoveIts = this.postLoveIts - 1;
+  onLoveIt() {}
+
+  onDontLoveIt() {}
+
+  // BUTTON DELETE
+  onDeletePost(post: Post) {
+    this.postsService.removePost(post);
   }
 
-  getColor() {
-    if (this.postLoveIts > 0) {
-      return 'green';
-    } else if (this.postLoveIts < 0) {
-      return 'red';
-    }
+  // getColor() {
+  //   if (this.postLoveIts > 0) {
+  //     return 'green';
+  //   } else if (this.postLoveIts < 0) {
+  //     return 'red';
+  //   }
+  // }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
   }
 }
